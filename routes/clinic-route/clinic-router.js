db65cc6a0908fc300119b4559b82cd90a60c5c8c
Alejandro Roman <aromanarguello@gmail.com>
2017-11-27 16:17:31 -0500

    work on user reviews *edit*.

--- routes/clinic-route/clinic-router.js
+++ routes/clinic-route/clinic-router.js
@@ -65,7 +65,7 @@ router.get("/user-reviews/:id", (req, res, next) => {
   })
   .then( userReviewResults => {
     res.locals.listOfUserReviews = userReviewResults;
-    res.render("/user-reviews/new");
+    res.render("user-reviews/new");
   })
   .catch( err => {
     next( err );
